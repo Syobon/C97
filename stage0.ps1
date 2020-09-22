0e997806bd478c083722f50b59e7e16f5ec7e8f6
Syobon <syobon@syobon.jp>
2020-09-22 20:30:50 +0900

    first commit

---
+++ stage0.ps1
@@ -0,0 +1,42 @@
+﻿#アセンブリ読み込み
+Add-Type -Assembly Microsoft.VisualBasic
+Add-Type -Assembly System.Windows.Forms
+
+#変数初期化
+$input_hostname = $null
+
+#変数代入
+$kick_powershell = "powershell.exe Set-ExecutionPolicy Bypass -Scope process -Force;powershell.exe "
+$script_path = "C:\work\scripts\stage1.ps1"
+$exec_line = $kick_powershell + $script_path
+
+#sysprep応答ファイルで追加したLocalAccounts
+$username = "setupuser"
+$password = "Passw0rd!"
+
+#PC名入力
+do {
+    do {
+        $input_hostname = [Microsoft.VisualBasic.Interaction]::InputBox("このコンピュータの名前を入力してください", "PC名の入力")
+        #入力されたホスト名が空白かチェック
+        $hostname_check = [String]::IsNullOrEmpty($input_hostname)
+        if($hostname_check -eq "True") {
+            [System.Windows.Forms.MessageBox]::Show("コンピュータ名が入力されていません。再度入力してください", "コンピュータ名が不正です", "OK", "Error","button1")
+        }
+    } while ($hostname_check -eq "True")
+    #PC名確定確認
+    $hostname_confirm = [System.Windows.Forms.MessageBox]::Show("コンピュータ名は" + $input_hostname + "でよいですか？", "コンピュータ名の確認", "YesNo", "Information","button1")
+} while ($hostname_confirm -eq "No")
+
+#ホスト名変更（Reboot pending）
+Rename-Computer -NewName $input_hostname
+
+#自動ログイン設定
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -PropertyType String -Value $username
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -PropertyType String -Value $password
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType String -Value 1
+
+#次回ログイン時にstage1.ps1実行
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name exec -PropertyType String -Value $exec_line
+
+Restart-Computer -Force
---
+++ stage1.ps1
@@ -0,0 +1,18 @@
+﻿#変数代入
+$DirectoryName = "hoge.local"
+#ユーザ名のみ　ドメインFQDN不要です。
+$DomainJoinUser = "hoge"
+$DomainJoinPassword = ConvertTo-SecureString -AsPlainText -Force "Passw0rd!"
+
+#AD参加
+$Credential = New-Object System.Management.Automation.PSCredential $DomainJoinUser, $DomainJoinPassword
+Add-Computer -DomainName $DirectoryName -Credential $Credential -Force -Options JoinWithNewName,AccountCreate
+
+#自動ログイン設定解除
+Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUser
+Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType String -Value 0
+
+#後片付け
+Remove-Item C:\work -Recurse
+Restart-Computer -Force
